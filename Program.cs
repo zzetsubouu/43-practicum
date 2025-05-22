bfc5302cacf54bcd0a94365c1c8cf614eb81c4dc
Kab-35-16 <Kab-35-16@KAB-31-7->
2025-05-22 10:14:33 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _43_practicum
+{
+    class Program
+    {
+        static void ClassTaker(MyClass myClass)
+        {
+            myClass.change = "изменено";
+        }
+
+        static void StructTaker(MyStruct myStruct)
+        {
+            myStruct.change = "изменено";
+        }
+
+        static void Main(string[] args)
+        {
+            MyClass classPrimer = new MyClass();
+            MyStruct structPrimer = new MyStruct();
+
+            classPrimer.change = "не изменено";
+            structPrimer.change = "не изменено";
+
+            ClassTaker(classPrimer);
+            StructTaker(structPrimer);
+
+            Console.WriteLine("Класс: " + classPrimer.change);
+            Console.WriteLine("Структура: " + structPrimer.change);
+
+            Console.ReadLine();
+        }
+    }
+}

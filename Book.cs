d5d775f5f60577e9a9e62de779b2986fefa572d2
Kab-35-16 <Kab-35-16@KAB-31-7->
2025-05-22 10:43:58 +0300

    3 задание

---
+++ Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _43_practicum
+{
+    struct Book
+    {
+        public string title;
+        public string authorsName;
+        public int releaseDate;
+        public int numberOfPages;
+
+        public Book(string title, string authorsName, int releaseDate, int numberOfPages)
+        {
+            this.title = title;
+            this.authorsName = authorsName;
+            this.releaseDate = releaseDate;
+            this.numberOfPages = numberOfPages;
+        }
+
+        public void Output()
+        {
+            Console.WriteLine($"Книга:\t{title}\tАвтор: {authorsName}\t Дата выпуска: {releaseDate}\t Количество страниц: {numberOfPages}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,20 @@ namespace _43_practicum
     {
         static void Main(string[] args)
         {
+            Book[] books = new Book[2];
+            string[] title = { "крутая книга", "Война и мир" };
+            string[] author = { "матвей", "Лев Толстой" };
+            int[] release = { 2025, 1867 };
+            int[] pages = { 5, 1300 };
+            for (int i = 0; i < books.Length; i++)
+            {
+                books[i].title = title[i];
+                books[i].authorsName = author[i];
+                books[i].releaseDate = release[i];
+                books[i].numberOfPages = pages[i];
+                books[i].Output();
+            }
+            Console.ReadKey();
         }
     }
 }
